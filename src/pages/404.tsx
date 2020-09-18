173a2ff34da4ca13a7482ca80eace8430ffc9cb0
John Koht <john@kohactive.com>
2020-09-17 21:51:17 -0500

    fix 404 page (for now)

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "components/layout/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <div class="404">
+    <SEO title="404: Not found" />
+    404
+  </div>
+)
+
+export default NotFoundPage
