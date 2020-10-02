ebf247aa546842615630b4a16be614cba3b0d6ac
John Koht <john@kohactive.com>
2020-10-01 22:18:40 -0500

    add open source link and fix footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -21,8 +21,8 @@ const Footer = () => (
     render={data => (
       <footer className="footer py-8 sm:py-12 bg-white">
         <div className="max-w-screen-xl px-4 md:px-0 mx-auto">
-          <div className="flex flex-wrap">
-            <span className="copyright mx-auto sm:m-0 sm:mr-auto text-sm font-thin text-gray-600">
+          <div className="flex flex-wrap flex-col sm:flex-row">
+            <span className="copyright my-3 mx-auto sm:m-0 sm:mr-auto text-gray-500">
               &copy; {new Date().getFullYear()} John Koht
             </span>
 
@@ -38,7 +38,15 @@ const Footer = () => (
               ))}
             </div>
 
-            <span className="phantom ml-auto hidden sm:flex sm:order-3"></span>
+            <span className="open-source m-auto sm:ml-auto sm:order-3">
+              <a
+                href="https://github.com/johnkoht/website"
+                target="_blank"
+                title="View the source code for my website"
+              >
+                Source Code
+              </a>
+            </span>
           </div>
         </div>
       </footer>
