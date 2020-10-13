9c42c05414bda469343203005151c346ee51eb39
John Koht <john@kohactive.com>
2020-10-12 20:36:27 -0500

    clean up the z-indices and transitions

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink";
+import { TransitionPortal } from "gatsby-plugin-transition-link";
 import Logo from "images/jk-logo.inline.svg"
 import BackBtn from "images/icons/back-btn.inline.svg"
 
@@ -15,37 +16,39 @@ const Header = ({ invert, headerBackBtn }: HeaderProps) => {
   let headerClass = invert ? "header invert" : "header"
 
   return (
-    <header className={headerClass}>
-      <div className="header-inner flex items-center px-4 md:px-10">
-        {headerBackBtn && (
+    <TransitionPortal level="top" className="header-wrapper">
+      <header className={headerClass}>
+        <div className="header-inner flex items-center px-4 md:px-10">
+          {headerBackBtn && (
+            <AniLink
+              cover
+              direction="left"
+              duration={.75}
+              bg="#06D6A0"
+              to={headerBackBtn}
+              className="header-back-btn"
+            >
+              <BackBtn alt="Back Button" />
+            </AniLink>
+          )}
+
+          {!headerBackBtn && <div className="header-spacer"></div>}
+
           <AniLink
             cover
-            direction="left"
+            direction="up"
             duration={.75}
-            bg="#06D6A0"
-            to={headerBackBtn}
-            className="header-back-btn"
+            bg="#59A5D8"
+            to="/"
+            className="ml-auto header-brand"
           >
-            <BackBtn alt="Back Button" />
+            <Logo alt="John Koht Logo" />
           </AniLink>
-        )}
 
-        {!headerBackBtn && <div className="header-spacer"></div>}
-
-        <AniLink
-          cover
-          direction="up"
-          duration={.75}
-          bg="#59A5D8"
-          to="/"
-          className="ml-auto header-brand"
-        >
-          <Logo alt="John Koht Logo" />
-        </AniLink>
-
-        <Navigation />
-      </div>
-    </header>
+          <Navigation />
+        </div>
+      </header>
+    </TransitionPortal>
   )
 }
 
