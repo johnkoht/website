2676367907634f1363018f70c33bee08332bd2df
John Koht <john@kohactive.com>
2021-01-12 20:31:24 -0600

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -204,5 +204,24 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-3980909-14",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "johnkoht.com",
+      },
+    },
   ],
 }
